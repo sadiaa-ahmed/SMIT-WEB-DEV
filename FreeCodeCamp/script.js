19a484d36a561ef46477ffc273a2a00c6deda200
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-09-04 22:20:00 -0700

    JS  Practise

---
+++ FreeCodeCamp/script.js
@@ -0,0 +1,38 @@
+// let a = 5;
+// for(i = 1 ; 1<=5 ; i++){
+//   let spaces = ' '.repeat(a-1);
+//   let hash = '#'.repeat(2 * i - 1);
+//   console.log(spaces + hash)
+// }
+
+// ********________*******
+//  related to varaible
+
+// let character;
+
+// let character = " Hello ";
+
+
+//  change double quote into single quote 
+// let character = 'Hello' ;
+
+
+// *****_______*****
+// let character = "Ali";
+// console.log(character);
+
+// ******________******
+// let character = "Hello";
+// character = "World";
+// console.log(character);
+
+// *****_______******
+// character = "Hello";
+// console.log(character);
+// character = "World";
+// console.log(character);
+
+// *****_______******
+
+// Use camel case to declare a new secondCharacter variable
+// let secondCharacter;
