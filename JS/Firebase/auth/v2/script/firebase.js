d1b97f40041d7d0e00b65989a9cbd48b33dc40e3
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-09-13 06:12:16 -0700

    Firebase

---
+++ JS/Firebase/auth/v2/script/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBp72WLTBHrac0vqKiBFcDxF_FNJP8PcOY",
+  authDomain: "smit-web-dev.firebaseapp.com",
+  projectId: "smit-web-dev",
+  storageBucket: "smit-web-dev.appspot.com",
+  messagingSenderId: "423692471251",
+  appId: "1:423692471251:web:f1db0ae0dc8b3bcc3d3d26"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
