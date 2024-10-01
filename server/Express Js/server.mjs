5f2822d1ae2ff292451f45c9256d6c8e33d03661
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-09-30 19:02:15 -0700

    server

---
+++ server/Express Js/server.mjs
@@ -0,0 +1,11 @@
+console.log("This is express server");
+
+
+
+// npm strt 
+
+// libraries :  nothing but  a bunch of function  Express.js library hai r aik framework      (Library main function ko call kerty hain)
+// r framework main syntax bhi thora framework ka hota hai
+// for expressJs installation :
+//            npm install express     
+//  expressJs k code ko run kerny k liye node (file ka naam)
---
+++ server/hello/main.mjs
@@ -0,0 +1,7 @@
+console.log("hello")
+
+
+// node -v 
+// code ko run k liye node (file ka naam)
+// npm init      (issy pakage.json ki file cretae hogi)
+ 
