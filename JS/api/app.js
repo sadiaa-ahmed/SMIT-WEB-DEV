ffb28d8dd86d23bdcaae0434e84eb5002a08d601
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-10-03 14:09:26 -0700

    Api

---
+++ JS/api/app.js
@@ -0,0 +1,5 @@
+//Top level await  -> means  await functions k baghair nahi challta tha  ab js main agaya hai k hum without function laga sekty hain await
+const response = await  fetch("https://api.github.com/users/sadiaahmed114");
+
+console.log(response);
+
