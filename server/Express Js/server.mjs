f4fc5e7aa8540de65fbd2fa8b55136aea6c34705
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-10-03 11:18:11 -0700

    Js

--- server/Express Js/server.mjs
+++ server/Express Js/server.mjs
@@ -2,10 +2,24 @@ console.log("This is express server");
 
 
 
-// npm strt 
+// npm start 
 
 // libraries :  nothing but  a bunch of function  Express.js library hai r aik framework      (Library main function ko call kerty hain)
 // r framework main syntax bhi thora framework ka hota hai
 // for expressJs installation :
 //            npm install express     
 //  expressJs k code ko run kerny k liye node (file ka naam)
+
+import express from 'express';
+const app = express()
+
+
+app.get('/profile', (req , res) =>{
+    console.log('this is profile!',  new Date());
+    res.send('This is profile' + new Date());
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT,()=>{
+    console.log(`Example app listening on port ${port}`)
+})
