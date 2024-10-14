f0fcf0ba04c5c2ad33caef857aa4782b66fe987e
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-10-14 06:38:50 -0700

    JS

--- JS/api/app.js
+++ JS/api/app.js
@@ -1,7 +1,7 @@
-//Top level await  -> means  await functions k baghair nahi challta tha  ab js main agaya hai k hum without function laga sekty hain await
-const response = await  fetch("https://api.github.com/users/sadiaahmed114");
+// //Top level await  -> means  await functions k baghair nahi challta tha  ab js main agaya hai k hum without function laga sekty hain await
+// const response = await  fetch("https://api.github.com/users/sadiaahmed114");
 
-console.log(response);
+// console.log(response);
 
 // rest apis  https://www.ibm.com/topics/rest-apis
 // status code https://restfulapi.net/http-status-codes
